8f846eb38b0c6df5161f152c46496c22eae90c1d
Andrew Furniss <drewfcs@gmail.com>
2018-07-29 21:21:57 -0400

    Optimized App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
 import Route from './routes/index.js';
 
-/*class App extends Component {
-  render() {
-    return (
-      <div>
-        <Title />
-        <div className = 'container'>
-          <Body />
-        </div>
-      </div>
-    );
-  }
-}*/
-
 export default () => (
   <Route />
 );
