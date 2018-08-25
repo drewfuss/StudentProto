384cd4a096224c03140ec01874b7fcac95e6818b
Andrew Furniss <drewfcs@gmail.com>
2018-08-24 20:33:52 -0400

    added listener to totalDonations in metadata

--- src/routes/views/funded/fundmeta.js
+++ src/routes/views/funded/fundmeta.js
@@ -5,19 +5,38 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import {db} from './../../../backend/firebase.js';
+import {onUpdateTotalDon} from './../../../actions/actions.js';
+import Snackbar from '@material-ui/core/Snackbar';
 
 class FundMeta extends Component
 {
+
+  constructor(props)
+  {
+    super(props);
+  }
+
+  componentWillMount()
+  {
+    db.collection("meta").doc("donations")
+    .onSnapshot({
+        // Listen for document metadata changes
+        includeMetadataChanges: true
+    }, function(doc) {
+      this.props.dispatch((onUpdateTotalDon(doc.data().totalDonations)));
+    }.bind(this));
+  }
+
   render()
   {
+    /*        <Typography color="primary">
+              <strong>Total People Funded: </strong>{this.props.totalFun}
+            </Typography>*/
     return (
       <div>
         <Typography color="primary">
           <strong>Total Amount Raised: </strong>{this.props.totalDon}
-        </Typography>
-        <Typography color="primary">
-          <strong>Total People Funded: </strong>{this.props.totalFun}
         </Typography>
       </div>
     )
